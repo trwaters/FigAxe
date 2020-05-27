a9c467299bb314304322901f4afcebce9901f12a
Tim Waters <tim.waters@unlv.edu>
2020-05-27 01:57:58 -0600

    initial commit of figaxe.py and gridspec_helper.py

---
+++ figaxe.py
@@ -0,0 +1,258 @@
+
+import numpy as np
+import pylab as plt
+import matplotlib
+from matplotlib.gridspec import GridSpec
+from importlib import reload
+import gridspec_helper; reload(gridspec_helper)
+
+# import style file
+plt.style.use(r"custom.mplstyle")
+
+
+
+### methods for lite layouts
+### ==================================================
+
+# use to choose among lite layouts in gridspec_helper
+def lite_layout(name=None, fig_size=None):
+    """
+    input: a string specifying the layout
+    returns: fig,axes handles
+    """
+
+    try:
+        layout = gridspec_helper.lite(name)
+        fig,axes = make_lite_figure(layout,fig_size)
+        return fig,axes
+    except:
+        print('[lite_layout]: exception thrown for layout {}.')
+        print("""
+            Example of a FigAxe layout array:
+            layout= [
+                    [1,1,1],
+                    [2,2,3],
+                    [4,5,3]
+                    ])
+
+            This corresponds to a 5-axes plot window like 
+
+            +-----------------+
+            |        1        |
+            +-----------------+
+            |      2    |     |
+            +-----+-----+  3  +
+            |  4  |  5  |     |
+            +-----+---+-------+
+ 
+            - row 1 layout is [1,1,1]
+            - row 2 layout is [2,2,3]
+            - row 3 layout is [4,5,3]   
+        """)
+
+
+
+# called by lite_layout() 
+def make_lite_figure(layout,fig_size=None):
+    """
+    input: an instance of LiteFigAxe 
+    returns: fig,axes handles
+    """
+
+    if fig_size == None:
+        fig_size = tuple(layout.size)
+
+    fig = plt.figure(figsize=fig_size)
+    gridspecs = gridspec_converter(layout.layout)
+    ax1 = fig.add_subplot(gridspecs[0])
+
+    axes_dic = {}
+    # The axes are stored in gridspec in the order specified in the layout_array
+    # so we just need to fill a dic with keys named '1','2','3',etc.
+    # Here we do that while accounting for the shared axes specification
+    for i,gridspec in enumerate(gridspecs):
+        axes_dic[str(i)] = fig.add_subplot(gridspec)
+
+
+    # add inward-pointing ticks on all 4 sides and set their visibility
+    for idx,key in enumerate(axes_dic.keys()):
+        ax = axes_dic[key]
+        ax.minorticks_on()
+        ax.xaxis.set_ticks_position('both')
+        ax.yaxis.set_ticks_position('both')
+        ax.tick_params(axis='both', direction='in',width=1.) 
+        ax.tick_params(which='minor',axis='both',direction='in',width=1.) 
+
+
+    # add cbar axes
+    Nax = len(gridspecs)
+    for n in range(Nax):
+        cbar_key = str(n+1) + 'cb'
+        axes_dic[cbar_key] = 1
+
+
+    plt.tight_layout()
+
+    return fig,axes_dic
+
+
+
+
+### methods for custom layouts
+### ==================================================
+
+# use to choose among lite layouts in gridspec_helper
+def custom_layout(name='default', fig_size=None):
+    """
+    input: a string specifying the layout
+    returns: fig,axes handles
+    """
+
+    try:
+        layout = gridspec_helper.custom(name)
+        fig,axes = make_figure(layout,fig_size)
+        return fig,axes
+    except:
+        print('Layout {} not found. Choose from these names:'.format(name))
+        show_layouts(show_art=False)
+
+
+
+# called by custom_layout()
+def make_figure(layout,fig_size=None):
+    """
+    input: an instance of LiteFigAxe 
+    returns: fig,axes handles
+    """
+
+    if fig_size == None:
+        fig_size = tuple(layout.size)
+
+    fig = plt.figure(figsize=fig_size)
+    gridspecs = gridspec_converter(layout.layout)
+    ax1 = fig.add_subplot(gridspecs[layout.head-1])
+
+    axes_dic = {str(layout.head): ax1}
+    # The axes are stored in gridspec in the order specified in the layout_array
+    # so we just need to fill a dic with keys named '1','2','3',etc.
+    # Here we do that while accounting for the shared axes specification
+    idx = 1
+    for gridspec,share_x,share_y in zip(gridspecs,layout.share_x,layout.share_y):
+        if gridspec != gridspecs[layout.head-1]:
+            if share_x > 0 and share_y > 0:
+                axes_dic[str(idx)] = fig.add_subplot(gridspec,sharex=ax1,sharey=ax1)
+            elif share_x > 0:
+                axes_dic[str(idx)] = fig.add_subplot(gridspec,sharex=ax1)
+            elif share_y > 0:
+                axes_dic[str(idx)] = fig.add_subplot(gridspec,sharey=ax1)
+            else:
+                axes_dic[str(idx)] = fig.add_subplot(gridspec)
+        idx += 1
+
+
+    # add inward-pointing ticks on all 4 sides and set their visibility
+    for idx,key in enumerate(axes_dic.keys()):
+        ax = axes_dic[key]
+        ax.minorticks_on()
+        ax.xaxis.set_ticks_position('both')
+        ax.yaxis.set_ticks_position('both')
+        ax.tick_params(axis='both', direction='in',width=1.) 
+        ax.tick_params(which='minor',axis='both',direction='in',width=1.) 
+
+        # hide axis tick labels
+        plt.setp(ax.get_xticklabels(), visible=not(layout.hide_x[idx]))
+        plt.setp(ax.get_yticklabels(), visible=not(layout.hide_y[idx]))
+
+
+    # add cbar axes
+    Nax = len(gridspecs)
+    for n in range(Nax):
+        if layout.cbars[n] == 1:
+            cbar_key = str(n+1) + 'cb'
+            axes_dic[cbar_key] = layout.cbars[n]
+
+
+    # finally, apply specifications defined in the layout
+    plt.subplots_adjust(left=layout.adjust[0], right=layout.adjust[1],
+                        bottom=layout.adjust[2],top=layout.adjust[3],
+                        wspace=layout.adjust[4],hspace=layout.adjust[5])
+
+    return fig,axes_dic
+
+
+
+
+### general methods
+### ==================================================
+
+# helper function to plot any layout in gridspec_helper
+def plot_layout(name='default'):
+
+    layouts_custom = gridspec_helper.custom_layouts()
+    layouts_lite = gridspec_helper.lite_layouts()
+
+    if name in layouts_custom.keys():
+        layout = gridspec_helper.custom(name)
+    elif name in layouts_lite.keys():
+        layout = gridspec_helper.lite(name)
+    else:
+        print('Layout {} not found. Choose from these names:'.format(name))
+        show_layouts(show_art=False)
+        import sys
+        sys.exit()
+
+    fig,axes = make_figure(layout)             
+    plt.show()
+
+
+
+# helper function to show layout names in gridspec_helper
+def show_layouts(show_art=True):
+
+    layouts_custom = gridspec_helper.custom_layouts()
+    layouts_lite = gridspec_helper.lite_layouts()
+
+    if show_art:
+        show_logo()
+
+    print('Custom layouts:\n======================')
+    for key in layouts_custom.keys():
+        layout = layouts_custom[key]
+        if show_art:
+            print('{}\n{}'.format(key,layout.art))
+        else:
+            print('{}'.format(key))
+
+    print('\nQuick layouts:\n======================')
+    for key in layouts_lite.keys():
+        print('{}'.format(key))
+
+
+
+# core method for utilizing gridspec with a layout array
+def gridspec_converter(layout):
+    layout = np.array(layout)
+    h, w = layout.shape
+    values_in_arr = sorted(set(layout.flatten()))
+    result = []
+    grid = GridSpec(h, w)
+    for v in values_in_arr:
+        xx, yy = ((layout - v) == 0).nonzero()
+        result.append(grid[xx.min(): xx.max() + 1,
+                           yy.min(): yy.max() + 1])
+    return result
+
+
+
+def show_logo():
+    print(str("""
+     ______  _                                  
+    (______)(_)         _______                 
+    (_)__    _   ____ ___    |___  ______       
+    (____)  (_) |    |__  /| |_  |/_/  _ \      
+    (_)     (_) |    |_  ___ |_>  < /  __/      
+    (_)     (_) |____|/_/  |_/_/|_| \___/ 
+                   (_)                      
+                   (_)                      
+                   (_)                      
+        """))
---
+++ gridspec_helper.py
@@ -0,0 +1,329 @@
+from dataclasses import dataclass
+# import numpy as np
+
+### Classes defining layouts
+### ==================================================
+
+@dataclass
+class LiteFigAxe:
+    size    : list
+    layout  : list
+
+
+@dataclass
+class FigAxe:
+    size    : list  # array of fig_size, e.g. [5,4] (row,col)
+    layout  : list  # arrays specifying gridspec layout
+    art     : str   # ascii art representing the layout
+    cbars   : list  # list of 1s,0s for cbar or no cbar
+    adjust  : list  # subplots_adjust params
+
+    # optional parameters
+    head    : int = 1       # specifies main plot in layout_array 
+    share_x : list = None   # list of 1s,0s; 1 means share head's x-axis
+    share_y : list = None   # list of 1s,0s; 1 means share head's y-axis
+    hide_x  : list = None   # list of 1s,0s; 1 means hide x-axis tick labels
+    hide_y  : list = None   # list of 1s,0s; 1 means hide y-axis tick labels
+
+    # by default, do not share limits of hide axes tick labels
+    def __post_init__(self, debug=False):
+        Npanels = max(max(self.layout))
+        zero_array = [0 for val in range(Npanels)]
+        if self.share_x is None:
+            self.share_x = zero_array
+        if self.share_y is None:
+            self.share_y = zero_array
+        if self.hide_x is None:
+            self.hide_x = zero_array
+        if self.hide_y is None:
+            self.hide_y = zero_array
+
+        if debug:
+            print("[__post_init__]:")
+            print(self.share_x)
+            print(self.hide_x)
+
+
+### methods utilzing above classes
+### ==================================================
+
+# use to quickly try out a new layout
+def lite_layouts():
+    """
+    Add your own custom figure size and layout below
+    for immediate use.  
+
+    Below is an example for how to make the following 
+    5-axes plot window 
+    +-------+---+-------+
+    |       |   2   | 3 |
+    +   1   +---+-------+
+    |       |   4   | 5 |
+    +-------+---+-------+
+
+    This layout is represented by an array of two arrays, 
+    - row 1 array is [1,1,2,2,3]
+    - row 2 array is [1,1,4,4,5]
+
+    Here's a different 5-axes plot window, which comes from
+    the matplotlib gallery for Gridspec
+    (google "Using Gridspec to make multi-column/row subplot layouts")
+
+    +-----------------+
+    |        1        |
+    +-----------------+
+    |      2    |     |
+    +-----+-----+  3  +
+    |  4  |  5  |     |
+    +-----+---+-------+
+
+    This layout is represented by an array of three arrays, 
+    - row 1 array is [1,1,1]
+    - row 2 array is [2,2,3]
+    - row 3 array is [4,5,3]   
+
+    Then quick_figure() will return the fig handle along
+    with a dictionary containing handles to these 5 axes.
+    """
+
+    layout_dic = {
+    'demo5a' : LiteFigAxe((10.,6.),
+        [
+        [1,1,2,2,3],
+        [1,1,4,4,5]
+        ]
+        ),
+    'demo5b' : LiteFigAxe((8.,6.),
+        [
+        [1,1,1],
+        [2,2,3],
+        [4,5,3]
+        ]
+        ),
+    }
+
+    return layout_dic
+        
+
+# use to add your frequently used layouts 
+def custom_layouts():
+
+    layout_dic = {
+    'default' : FigAxe((7.,7.),
+        [
+        [1]
+        ],
+        str("""
+        +---+
+        | 1 |
+        +---+
+        """),
+        [0],    # colorbars
+        [0.08,0.99,0.07,0.94,0.02,0.03]  # params: L,R,bot,top,wspace,hspace
+        ),
+    'h1' : FigAxe((7.,7.),
+        [
+        [1]
+        ],
+        str("""
+        +---+
+        | 1 |c
+        +---+
+        """),
+        [1],    # colorbars
+        [0.08,0.99,0.07,0.94,0.02,0.03]  # params: L,R,bot,top,wspace,hspace
+        ),
+    'h2' : FigAxe((8.5,5.1),
+        [
+        [1,2]
+        ],
+        str("""
+          c   c 
+        +---+---+
+        | 1 | 2 |
+        +---+---+
+        """),
+        [1,1],  # colorbars
+        [0.05,0.99,0.07,0.9,0.02,0.03],  # params: L,R,bot,top,wspace,hspace
+
+        # enable these optionals
+        share_x = [0,1],  # panel 2 shares xlim of panel 1
+        share_y = [0,1],  # panel 2 shares ylim of panel 1
+        hide_y =  [0,1],  # hide y-tick labels on panel 2
+        ),
+    'h2cb1' : FigAxe((8.,5.),
+        [
+        [1,2]
+        ],
+        str("""
+        +---+---+ 
+        | 1 | 2 |c
+        +---+---+
+        """),
+        [0,1],  # colorbars
+        [0.05,0.99,0.07,0.9,0.02,0.03],  # params: L,R,bot,top,wspace,hspace
+
+        # enable these optionals
+        share_x = [0,1],  # panel 2 shares xlim of panel 1
+        share_y = [0,1],  # panel 2 shares ylim of panel 1
+        hide_y =  [0,1],  # hide y-tick labels on panel 2
+        ),
+    'h3' : FigAxe((10.,4.2),
+        [         # layout_array
+        [1,2,3]
+        ],
+        str("""
+          c   c   c  
+        +---+---+---+
+        | 1 | 2 | 3 |
+        +---+---+---+
+        """),
+        [1,1,1],  # colorbars
+        [0.04,0.99,0.06,0.91,0.02,0.03],  # params: L,R,bot,top,wspace,hspace
+
+        # enable these optionals
+        share_x = [0,1,1],  # panels 2,3 share xlim of panel 1
+        share_y = [0,1,1],  # panels 2,3 share ylim of panel 1
+        hide_y =  [0,1,1],  # hide y-tick labels on panels 2 and 3
+        ),
+    'h3cb1' : FigAxe((10.5,3.7),
+        [         # layout_array
+        [1,2,3]
+        ],
+        str("""
+        +---+---+---+
+        | 1 | 2 | 3 |c
+        +---+---+---+
+        """),
+        [0,0,1],  # colorbars
+        [0.04,0.94,0.06,0.98,0.01,0.03],  # params: L,R,bot,top,wspace,hspace
+
+        # enable these optionals
+        share_x = [0,1,1],  # panels 2,3 share xlim of panel 1
+        share_y = [0,1,1],  # panels 2,3 share ylim of panel 1
+        hide_y =  [0,1,1],  # hide y-tick labels on panels 2 and 3
+        ),
+    'h4' : FigAxe((12.,4.2),
+        [         # layout_array
+        [1,2,3,4]
+        ],
+        str("""
+          c   c   c   c
+        +---+---+---+---+
+        | 1 | 2 | 3 | 4 |
+        +---+---+---+---+
+        """),
+        [1,1,1],  # colorbars
+        [0.04,0.99,0.06,0.91,0.02,0.03],  # params: L,R,bot,top,wspace,hspace
+
+        # enable these optionals
+        head    = 2,          # make panel 2 the head axes 
+        share_x = [0,0,1,1],  # panels 2,3 share xlim of panel 1
+        share_y = [0,0,1,1],  # panels 2,3 share ylim of panel 1
+        hide_y =  [0,0,1,1],  # hide y-tick labels on panels 2 and 3
+        ),
+    'h4cb1' : FigAxe((10.5,3.7),
+        [         # layout_array
+        [1,2,3]
+        ],
+        str("""
+        +---+---+---+
+        | 1 | 2 | 3 |c
+        +---+---+---+
+        """),
+        [0,0,1],  # colorbars
+        [0.04,0.94,0.06,0.98,0.01,0.03],  # params: L,R,bot,top,wspace,hspace
+
+        # enable these optionals
+        share_x = [0,1,1],  # panels 2,3 share xlim of panel 1
+        share_y = [0,1,1],  # panels 2,3 share ylim of panel 1
+        hide_y =  [0,1,1],  # hide y-tick labels on panels 2 and 3
+        ),
+    'v3cb1' : FigAxe((4.,10.5),
+        [         # layout_array
+        [1],
+        [2],
+        [3]
+        ],
+        str("""
+          c  
+        +---+
+        | 1 | 
+        +---+
+        | 2 |
+        +---+
+        | 3 |
+        +---+
+        """),
+        [1,0,0],  # colorbars
+        [0.12,0.98,0.05,0.95,0.02,0.05],  # params: L,R,bot,top,wspace,hspace
+
+        # enable these optionals 
+        share_x = [0,1,1],  # panels 2,3 share xlim of panel 1
+        share_y = [0,1,1],  # panels 2,3 share ylim of panel 1
+        hide_x =  [1,1,0],  # hide x-tick labels on panels 1 and 2
+        )
+    } # close
+                # club4am_layout('h4',          (10.,4.),
+                #                               [
+                #                               [1,2,3]
+                #                               ],
+                #                           str("""
+                #                           +---+---+---+---+
+                #                           | 1 | 2 | 3 | 4 |
+                #                           +---+---+---+---+
+                #                           """)
+                #                               ),
+                # club4am_layout('h5',          (12.,4.),
+                #                               [
+                #                               [1,2,3]
+                #                               ],
+                #                           str("""
+                #                           +---+---+---+---+---+
+                #                           | 1 | 2 | 3 | 4 | 5 |
+                #                           +---+---+---+---+---+
+                #                           """)
+                #                               ),
+                # club4am_layout('h6',          (14.,4.),
+                #                               [
+                #                               [1,2,3]
+                #                               ],
+                #                           str("""
+                #                           +---+---+---+---+---+---+
+                #                           | 1 | 2 | 3 | 4 | 5 | 6 |
+                #                           +---+---+---+---+---+---+
+                #                           """)
+                #                               ),
+                # club4am_layout('g3',          (6.,4.),
+                #                               [
+                #                               [1, 1, 3],
+                #                       [2, 2, 3],
+                #                       [2, 2, 3],
+                #                               ],
+                #                           str("""
+                #                           +------------+----+
+                #                           |     1      |    |
+                #                           | ---------- |    |
+                #                           |            |  3 |
+                #                           |     2      |    |
+                #                           |            |    |
+                #                           +------------+----+
+                #                           """)
+                                           # )
+
+    return layout_dic
+
+
+### implementation of a case-switch to choose a layout
+### ==================================================
+
+def lite(name):
+    if name is None:
+        name = 'demo5b'
+    layout_dic = lite_layouts()
+    return layout_dic.get(name, '[quick_layout]: layout {} not found.'.format(name))
+
+
+def custom(name):
+    layout_dic = custom_layouts()
+    return layout_dic.get(name, '[custom_layout]: layout {} not found.'.format(name))
